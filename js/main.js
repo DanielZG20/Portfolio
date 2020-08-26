0c2c25dd9b0ede2821c583ba8be3f25e289df961
Daniel Zanoni Garcia <55899099+DanielZG20@users.noreply.github.com>
2020-08-26 19:14:45 -0300

    att

--- js/main.js
+++
@@ -1,7 +0,0 @@
-const toggle = document.getElementById('icon-toggle');
-
-toggle.addEventListener('click', function () {
-  const navbar_content = document.getElementById('navbar-content')
-  navbar_content.classList.add('active')
-})
-
