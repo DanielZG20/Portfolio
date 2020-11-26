23063498ad2bb7252ce65b3ac2222ea1df894081
Daniel Zanoni Garcia <55899099+DanielZG20@users.noreply.github.com>
2020-11-26 18:49:38 -0300

    down

--- js/main.js
+++
@@ -1,6 +0,0 @@
-const toggleButton = document.getElementById('toggle-button');
-const navbarLinks = document.getElementById('navbar-links');
-
-toggleButton.addEventListener('click', () => {
-  navbarLinks.classList.toggle('active')
-});
